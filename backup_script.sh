bf0a2c16c8ae15f56429400f899463cd3368049b
daebenji <ev.fools@gmail.com>
2017-10-13 14:42:09 +0200

    2

---
+++ backup_script.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+LOCATION=""
+logfile="/var/log/backup.log"
+
+backup_dir="/var/www/vhosts/lvps5-35-240-56.dedicated.hosteurope.de/backup_mindmeister/backup"
+
+echo "backup.sh start $(date)" >> "${logfile}"
+
+if [ $(date -d tomorrow +"%e") == 1 ]; then
+  backup_dir=${backup_dir}/monthly
+  mv $LOCATION/*.zip $backup_dir/backup_mindmeister.zip
+
+  success=$?
+  if [[ $success -eq 0 ]];
+  then
+      echo " OK: $(date): Backup wurde erfoglreich in $backup_dir abgelegt." >> "${logfile}"
+  else
+      echo " FEHLER: $(date): Backup konnte auf ${INSTANCE} nicht erstellt werden." >> "${logfile}"
+  fi
+
+else
+
+  backup_dir=${backup_dir}/"daily_"$(date +"%u")
+  mv $LOCATION/*.zip $backup_dir/backup_mindmeister.zip
+
+  success=$?
+  if [[ $success -eq 0 ]];
+  then
+      echo " OK: $(date): Backup wurde erfoglreich in $backup_dir abgelegt." >> "${logfile}"
+  else
+      echo " FEHLER: $(date): Backup konnte nicht aufgeführt werden." >> "${logfile}"
+  fi
+fi
