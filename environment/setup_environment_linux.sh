c180d98a972dfdd21aca9c3e18f7c84053ca2bd2
daebenji <ev.fools@gmail.com>
2018-02-28 09:57:42 +0100

    Update setup_environment_linux.sh

--- environment/setup_environment_linux.sh
+++ environment/setup_environment_linux.sh
@@ -1,15 +1,43 @@
-apt update && apt upgrade
-apt install vim git sudo
+#!/bin/bash
+# Init
+FILE="/tmp/out.$$"
+GREP="/bin/grep"
 
-# Public Key
-mkdir .ssh
-wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+# check if user or root environment
+if [[ $EUID -ne 0 ]]; then
+ 
+   # public key
+   mkdir .ssh
+   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+   
+   # bashrc
+   https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_users -O .bashrc
+   source .bashrc
+   
+   # vimrc
+   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   vim +PluginInstall +qall
+   
+   exit 0
+   
+ else
+   
+   apt update && apt upgrade
+   apt install vim git sudo
+   
+   # public key
+   mkdir .ssh
+   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+   
+   # bashrc
+   https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O .bashrc
+   source .bashrc
+   
+   # vimrc
+   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   vim +PluginInstall +qall
+   exit 0   
+fi
 
-# Bashrc
-wget https://github.com/daebenji/scripts/blob/master/environment/.bashrc_root -O .bashrc
-source .bashrc
-
-# vimrc
-wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-vim +PluginInstall +qall
