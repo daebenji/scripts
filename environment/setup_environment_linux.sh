424690367b91ba72b58e4905b42b6c3a1997d7e0
daebenji <ev.fools@gmail.com>
2018-02-28 09:39:21 +0100

    Create setup_environment_linux.sh

---
+++ environment/setup_environment_linux.sh
@@ -0,0 +1,15 @@
+apt update && apt upgrade
+apt install vim git
+
+# Public Key
+mkdir .ssh
+wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+
+# Bashrc
+wget https://github.com/daebenji/scripts/blob/master/environment/.bashrc_root -O .bashrc
+source .bashrc
+
+# vimrc
+wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
