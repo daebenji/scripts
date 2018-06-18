5606e441f980dec6a74f9e2815fc7c8ab207f672
daebenji <ev.fools@gmail.com>
2018-06-18 14:44:23 +0200

    Create setup_cfengine_hub_and_host.sh

---
+++ vagrant/setup_cfengine_hub_and_host.sh
@@ -0,0 +1,4 @@
+wget https://cfengine-package-repos.s3.amazonaws.com/enterprise/Enterprise-3.10.4/misc/CFEngine_Enterprise_vagrant_quickstart-3.10.4-1.tar.gz 
+tar zxfv CFEngine_Enterprise_vagrant_quickstart-3.10.4-1.tar.gz 
+rm CFEngine_Enterprise_vagrant_quickstart-3.10.4-1.tar.gz 
+cd CFEngine_Enterprise_vagrant_quickstart_3.10.4/
