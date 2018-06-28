869fe2dd1f489d4a6198ce939f59e5db1ed96db3
root <root@scotty.geogr.uni-jena.de>
2018-06-28 10:09:01 +0200

    modify sourcing

--- environment/setup_environment_linux.sh
+++ environment/setup_environment_linux.sh
@@ -12,7 +12,7 @@ if [[ $EUID -ne 0 ]]; then
    
    # bashrc
    wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_users -O .bashrc
-   source .bashrc
+   . ~/.bashrc
    
    # vimrc
    wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
@@ -24,49 +24,49 @@ if [[ $EUID -ne 0 ]]; then
  else
    
    if [[ -f /etc/debian_version ]]; then 
-	   apt update && apt upgrade 
-	   apt install vim git sudo 
+	   #apt update && apt upgrade 
+	   #apt install vim git sudo 
 	   
 	   # add user to sudoers without asking for password
-   	   # echo "<username>    ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+	   # echo "<username>	 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
  
-           # public key
-   	   mkdir .ssh
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
-   	   
-   	   # .bashrc
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
-   	   source /root/.bashrc
-   	   
-   	   # .profile
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
-   	   source /root/.profile
-   	   
-   	   # .vimrc
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
-   	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-   	   vim +PluginInstall +qall
-   	   exit 0   
+		   # public key
+	   mkdir .ssh
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+	   
+	   # .bashrc
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
+	   . ~/.bashrc
+	   
+	   # .profile
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
+	   . ~/.profile
+	   
+	   # .vimrc
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	   vim +PluginInstall +qall
+	   exit 0	
    else
 	   pacman -Suy
 	   pacman -S vim git
-           # public key
-   	   mkdir .ssh
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
-   	   
-   	   # .bashrc
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
-   	   source /root/.bashrc
-   	   
-   	   # .profile
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
-   	   source /root/.profile
-   	   
-   	   # .vimrc
-   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
-   	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-   	   vim +PluginInstall +qall
-   	   exit 0   
+		   # public key
+	   mkdir .ssh
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+	   
+	   # .bashrc
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
+	   . ~/.bashrc
+	   
+	   # .profile
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
+	   . ~/.profile
+	   
+	   # .vimrc
+	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+	   vim +PluginInstall +qall
+	   exit 0	
 
    fi
 fi
