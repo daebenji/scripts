153275f8eb26506d0c15aab7e9309bffd289577a
daebenji <daebenji@mail.com>
2018-08-10 14:01:40 +0200

    removed args, replaced with ipconfig

--- ansible/scripts/get_mac_from_windows.sh
+++ ansible/scripts/get_mac_from_windows.sh
@@ -2,7 +2,7 @@
 
 [ $# -eq 0 ] && { echo "Usage: $0 <host/hostgroup> <win_command> i.e. $0 windowsserver ipconifg /all"; exit 1; }
 
-ARGS="${*:2}"
+#ARGS="${*:2}"
 
-ansible $1 -m win_command -a "$ARGS" | grep -i physische | awk -F ':' '{print $2}' | sed 's/-/:/g' | tr '[:upper:]' '[:lower:]'
+ansible $1 -m win_command -a "ipconfig /all" | grep -i physische | awk -F ':' '{print $2}' | sed 's/-/:/g' | tr '[:upper:]' '[:lower:]'
 
