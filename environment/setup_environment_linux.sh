da3ca3783a28f9d6e5b15c49eaac69b18bb220b2
daebenji <ev.fools@gmail.com>
2018-09-03 15:23:28 +0200

    foobar

--- environment/setup_environment_linux.sh
+++ environment/setup_environment_linux.sh
@@ -2,7 +2,6 @@
 # Init
 FILE="/tmp/out.$$"
 GREP="/bin/grep"
-VIMRUNTIME="/usr/share/vim/vim81/"
 
 # check if user or root environment
 if [[ $EUID -ne 0 ]]; then
@@ -13,73 +12,66 @@ if [[ $EUID -ne 0 ]]; then
    
    # bashrc
    wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_users -O .bashrc
-   . ~/.bashrc
+   source .bashrc
 
-   # color-scheme gruvbox
-   wget https://raw.githubusercontent.com/daebenji/gruvbox/master/colors/gruvbox.vim -O $VIMRUNTIME/colors/gruvbox.vim
+   # .profile
+   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O ~/profile
+   source ~/.profile
    
    # vimrc
    wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-   vim +BundleInstall +qall
-   # vim +PluginInstall +qall
+   vim +PluginInstall +qall
    
    exit 0
    
  else
    
    if [[ -f /etc/debian_version ]]; then 
-	   #apt update && apt upgrade 
-	   #apt install vim git sudo 
+	   apt update && apt upgrade 
+	   apt install vim git sudo 
 	   
 	   # add user to sudoers without asking for password
-	   # echo "<username>	 ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+   	   # echo "<username>    ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
  
-	   # public key
-	   mkdir .ssh
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
-	   
-	   # .bashrc
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
-	   . ~/.bashrc
-	   
-	   # .profile
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
-	   . ~/.profile
-
-       # color-scheme gruvbox
-       sudo wget https://raw.githubusercontent.com/daebenji/gruvbox/master/colors/gruvbox.vim -O $VIMRUNTIME/colors/gruvbox.vim
-	   
-	   # .vimrc
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
-	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	   vim +BundleInstall +qall
-           # vim +PluginInstall +qall
-	   exit 0	
+           # public key
+   	   mkdir .ssh
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+   	   
+   	   # .bashrc
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
+   	   source /root/.bashrc
+   	   
+   	   # .profile
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
+   	   source /root/.profile
+   	   
+   	   # .vimrc
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+   	   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   	   vim +PluginInstall +qall
+   	   exit 0   
    else
 	   pacman -Suy
 	   pacman -S vim git
-		   # public key
-	   mkdir .ssh
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
-	   
-	   # .bashrc
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
-	   . ~/.bashrc
-	   
-	   # .profile
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
-	   . ~/.profile
-	   
-       # color-scheme gruvbox
-       wget https://raw.githubusercontent.com/daebenji/gruvbox/master/colors/gruvbox.vim -O $VIMRUNTIME/colors/gruvbox.vim
+           # public key
+   	   mkdir .ssh
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/id_rsa_pub -O .ssh/authorized_keys
+   	   
+   	   # .bashrc
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.bashrc_root -O /root/.bashrc
+   	   source /root/.bashrc
+   	   
+   	   # .profile
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/root_profile.txt -O /root/.profile
+   	   source /root/.profile
+   	   
+   	   # .vimrc
+   	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
+   	   git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+   	   vim +PluginInstall +qall
+   	   exit 0   
 
-	   # .vimrc
-	   wget https://raw.githubusercontent.com/daebenji/scripts/master/environment/.vimrc_linux_unix -O .vimrc
-	   git clone https://github.com/daebenji/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	   vim +BundleInstall +qall
-           # vim +PluginInstall +qall
-	   exit 0	
    fi
 fi
 
