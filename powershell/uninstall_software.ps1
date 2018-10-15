5b05d66bf2d129a788376f1850ba6873f64f69ed
daebenji <ev.fools@gmail.com>
2018-10-15 11:39:12 +0200

    Update uninstall_software.ps1

--- powershell/uninstall_software.ps1
+++ powershell/uninstall_software.ps1
@@ -1,5 +1,4 @@
-$app = Get-WmiObject -Class Win32_Product | Where-Object {
-$_.Name -match “Oracle VM VirtualBox 5.1.14”
+$app = Get-WmiObject -Class Win32_Product -Filter "name like '%SoftwareName%'" 
 }
 
 $app.Uninstall()
