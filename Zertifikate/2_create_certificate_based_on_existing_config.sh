bd8f4e7b8c5c7348bd92c8acd0f7343c229804e9
daebenji <ev.fools@gmail.com>
2019-07-08 15:05:59 +0200

    foo

---
+++ Zertifikate/2_create_certificate_based_on_existing_config.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source config.txt
+
+if [ -z "$REQ_CN" ]
+then
+  if [ $# -eq 0 ]
+  then
+    echo "Usage: $0 servername i.e. server.organisation.tld || or enter REQ_CN in config.txt"
+    exit 1
+  else
+    SERVERNAME=$1
+  fi
+else
+  SERVERNAME=$REQ_CN
+fi
+
+if ([[ ! -d $SERVERNAME ]] && [[ ! -f $SERVERNAME.$(date +%Y).key ]] && [[ $SERVERNAME.conf ]])
+then
+  echo "ERROR: Folder $SERVERNAME, Key $SERVERNAME.$(date +%Y).key or Config $SERVERNAME.conf does not exist."
+else
+  cd $SERVERNAME
+  ### CREATE CERTIFICATE BASED ON EXISTING CSR AND KEY
+  openssl req -new -x509 -key $SERVERNAME.$(date +%Y).key -config $SERVERNAME.conf -out $SERVERNAME.$(date +%Y).crt -days 730
+fi
