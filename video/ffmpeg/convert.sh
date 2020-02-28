4b9f6dc74d214e6b6138a44c0aba9887a6f0d38c
daebenji <ev.fools@gmail.com>
2020-02-28 08:38:27 +0100

    add scale and pix_fmt

--- video/ffmpeg/convert.sh
+++ video/ffmpeg/convert.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-inputFile=$1
-[[ -z $inputFile ]] && echo "Keine Datei zum konvertieren angegeben"
-[[ -z $inputFile ]] && echo "Error: usage ./convert.sh <movie> ec:1" && exit 1
+set -x
 
-[[ ! -f $inputFile ]] && echo "Error: $inputFile not found ec:2" && exit 2
-outputFile=$(echo $inputFile | sed 's/.mkv/-AC3-convert.mkv/g;s/.mp4/-AC3-convert.mp4/g')
-
-aCodec="libmp3lame"
-vCodec="libx264"
-sample="-ss 0 -t 120"
-videoOption="-preset slow -tune film -profile:v high10 -level 4.1 -crf 19"
-audioTrack=$(mediainfo --Output=JSON $inputFile | \
-	sed -n "/StreamOrder/,/Language/p"  | \
-	grep -E 'StreamOrder|Lang|"Format"' | \
-	sed "s/,//g;s/ //g;s/\"//g;1,2d" | \
-	sed '/Stream/N;y/\n/\t/;/Format/N;y/\n/\t/' | \
-	dmenu -l 5 | \
-	cut -d\: -f2 | \
-	sed 's/Format//')
-audioTrack=2
-echo "Using AudioTrack: $audioTrack"
-
-ffmpeg -i $inputFile $sample -map 0 -vcodec $vCodec $videoOption -map 0:$audioTrack -scodec copy -acodec $aCodec -b:a 640k $outputFile
+videoOptions="-vf scale=1920:-1 -preset slow -tune film -profile:v high -pix_fmt yuv420p -level 4.1 -crf 19"
+audioOptions='-b:a 640k'
+vCodec=libx264
+aCodec=ac3
+case "$1" in
+	"-s")
+		outputFile=$(echo $inputFile | sed "s/.mp4/-sample.mp4/")
+		sample="-ss 360 -t 50"
+		inputFile=$2
+		[[ -z $inputFile ]] && echo "Keine Datei zum konvertieren angegeben"
+		[[ -z $inputFile ]] && echo "Error: usage ./convert.sh <movie> ec:1" && exit 1
+		[[ ! -f $inputFile ]] && echo "Error: $inputFile not found ec:2" && exit 2
+		;;
+	*)
+		inputFile=$1
+		[[ -z $inputFile ]] && echo "Keine Datei zum konvertieren angegeben"
+		[[ -z $inputFile ]] && echo "Error: usage ./convert.sh <movie> ec:1" && exit 1
+		[[ ! -f $inputFile ]] && echo "Error: $inputFile not found ec:2" && exit 2
+		;;
+esac
+[[ -z $sample ]] && outputFile=$(echo $inputFile | sed "s/.mp4/-converted.mp4/")
+ffmpeg -i $inputFile $sample -map 0:0 -vcodec $vCodec $videoOptions -map 0:1 -acodec $aCodec $audioOptions -scodec copy $outputFile
